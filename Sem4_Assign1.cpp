7f91b1efa46e28ddbf8a99c5cf34b9c133a19c3e
tevil344 <84931775+tevil344@users.noreply.github.com>
2025-01-15 13:30:54 +0530

    oh

--- Sem4_Assign1.cpp
+++ Sem4_Assign1.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
+struct Node {
     int data;
     Node *left, *right;
-    Node(int d){
+    Node(int d) {
         data = d;
         left = nullptr;
         right = nullptr;
     }
 };
 
-int main(){
-    // Initilize and allocate memory for tree nodes
+int height(Node *root) {
+    if (root == nullptr)
+        return -1;
+
+    // compute the height of left and right subtrees
+    int lHeight = height(root->left);
+    int rHeight = height(root->right);
+
+    return max(lHeight, rHeight) + 1;
+}
+
+int main() {
+    // Initialize and allocate memory for tree nodes
     Node* firstNode = new Node(2);
     Node* secondNode = new Node(3);
     Node* thirdNode = new Node(4);
@@ -22,5 +33,8 @@ int main(){
     firstNode->left = secondNode;
     firstNode->right = thirdNode;
     secondNode->left = fourthNode;
+
+    // Pass the root node to the height function
+    cout << height(firstNode);
     return 0;
 }
